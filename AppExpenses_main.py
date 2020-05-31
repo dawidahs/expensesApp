bea853dc05191fd0bce94fe7f6e5a78cf970254a
DavidHS <dawidhdez@gmail.com>
2020-05-31 14:34:43 +0200

    first commit

--- AppExpenses_main.py
+++ AppExpenses_main.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 class MyApp(App):
 
     def build(self):
-        return Label(text='DHS Accountancy -1')
+        return Label(text='DHS Accountancy 1')
 
 if __name__ == '__main__':
     MyApp().run()
