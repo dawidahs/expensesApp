5408c28bbfd83317ec644b670d865a9210db0f37
DavidHS <dawidhdez@gmail.com>
2020-06-03 01:23:58 +0200

    changed file

--- pdfToJpg_coverter.py
+++ pdfToJpg_coverter.py
@@ -1,6 +1,11 @@
 from pdf2image import convert_from_path
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
 
-pages = convert_from_path('C:\\Users\\david\\Downloads\\1_Personal+Statement+Bulk+Print+1590664876968.pdf', 500)
+pages = convert_from_path('C:\\Users\\david\\Downloads\\BorradorDeclaracion2019.pdf', dpi=500, single_file=False)
 
 for page in pages:
     page.save('G:\\My Drive\\Contabilidad\\Gastos\\Expense_Receipts\\out.jpg', 'JPEG')
