f7961c68a15a879d3537ff7c89f8f5a72c44658f
DavidHS <dawidhdez@gmail.com>
2020-06-03 00:43:44 +0200

    Change destination folder

--- pdfToJpg_coverter.py
+++ pdfToJpg_coverter.py
@@ -3,4 +3,4 @@ from pdf2image import convert_from_path
 pages = convert_from_path('C:\\Users\\david\\Downloads\\1_Personal+Statement+Bulk+Print+1590664876968.pdf', 500)
 
 for page in pages:
-    page.save('out.jpg', 'JPEG')
+    page.save('G:\\My Drive\\Contabilidad\\Gastos\\Expense_Receipts\\out.jpg', 'JPEG')
