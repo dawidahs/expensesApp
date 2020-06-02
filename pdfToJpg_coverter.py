420a48ca91ed1e7c1bbba40858a9558bd66a8802
DavidHS <dawidhdez@gmail.com>
2020-06-03 01:37:07 +0200

    Added output folder and name

--- pdfToJpg_coverter.py
+++ pdfToJpg_coverter.py
@@ -5,7 +5,8 @@ from pdf2image.exceptions import (
     PDFSyntaxError
 )
 
-pages = convert_from_path('C:\\Users\\david\\Downloads\\BorradorDeclaracion2019.pdf', dpi=500, single_file=False)
 
-for page in pages:
-    page.save('G:\\My Drive\\Contabilidad\\Gastos\\Expense_Receipts\\out.jpg', 'JPEG')
+pages = convert_from_path('C:\\Users\\david\\Downloads\\BorradorDeclaracion2019.pdf', dpi=500, output_folder='G:\\My Drive\\Contabilidad\\Gastos\\Expense_Receipts\\', fmt='jpg', single_file=False, output_file='BorradorDeclaracion2019')
+
+#for page in pages:
+    #page.save('G:\\My Drive\\Contabilidad\\Gastos\\Expense_Receipts\\out.jpg', 'JPEG')
