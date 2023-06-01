54263b9c9af34a9c15d5a8bd67c65ebb2bce66fb
dawidahs <46559351+dawidahs@users.noreply.github.com>
2023-06-01 11:05:15 +0200

    Update pdfToJpg_coverter.py

--- pdfToJpg_coverter.py
+++ pdfToJpg_coverter.py
@@ -6,7 +6,7 @@ from pdf2image.exceptions import (
 )
 
 
-pages = convert_from_path('C:\\Users\\david\\Downloads\\BorradorDeclaracion2019.pdf', dpi=500, output_folder='G:\\My Drive\\Contabilidad\\Gastos\\Expense_Receipts\\', fmt='jpg', single_file=False, output_file='BorradorDeclaracion2019')
+pages = convert_from_path('\BorradorDeclaracion2019.pdf', dpi=500, output_folder='G:\\My Drive\\Contabilidad\\Gastos\\Expense_Receipts\\', fmt='jpg', single_file=False, output_file='BorradorDeclaracion2019')
 
 #for page in pages:
     #page.save('G:\\My Drive\\Contabilidad\\Gastos\\Expense_Receipts\\out.jpg', 'JPEG')
